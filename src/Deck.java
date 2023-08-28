9e402dfe2759d98ebef705f58e949c0bb0a8d1dd
joaofiRocha <joao.farah@edu.pucrs.br>
2023-08-28 11:45:05 -0300

    uno

--- src/Deck.java
+++ src/Deck.java
@@ -21,6 +21,7 @@ public class Deck {
     }
 
     public void showDeck(){
+        Order();
         System.out.println("\u001B[0mYour cards:");
         System.out.print(" | ");
         for(int i = 0; i < numCards; i++){
@@ -60,6 +61,72 @@ public class Deck {
             numCards++;
             j++;
 
+        }
+    }
+
+    private void Order(){
+        int nRed = 0;
+        int nBlue = 0;
+        int nGreen = 0;
+        int nYellow = 0;
+        int nSpecial = 0;
+
+        // Count the number of each color and special cards
+        for (int i = 0; i < numCards; i++) {
+            if (getCardColor(i) == 0) {
+                nRed++;
+            } else if (getCardColor(i) == 1) {
+                nBlue++;
+            } else if (getCardColor(i) == 2) {
+                nGreen++;
+            } else if (getCardColor(i) == 3) {
+                nYellow++;
+            } else if (getCardType(i) > 0) {
+                nSpecial++;
+            }
+        }
+
+        Card[] red = new Card[nRed];
+        Card[] blue = new Card[nBlue];
+        Card[] green = new Card[nGreen];
+        Card[] yellow = new Card[nYellow];
+        Card[] special = new Card[nSpecial];
+
+        // Separate cards into respective color arrays
+        for (int i = 0; i < numCards; i++) {
+            if (getCardColor(i) == 0) {
+                red[--nRed] = card[i];
+            }
+            else if (getCardColor(i) == 1) {
+                blue[--nBlue] = card[i];
+            }
+            else if (getCardColor(i) == 2) {
+                green[--nGreen] = card[i];
+            }
+            else if (getCardColor(i) == 3) {
+                yellow[--nYellow] = card[i];
+            }
+            else if (getCardType(i) > 0) {
+                special[--nSpecial] = card[i];
+            }
+        }
+
+        // Reorder the card array based on color and type
+        int index = 0;
+        for (int i = 0; i < red.length; i++) {
+            card[index++] = red[i];
+        }
+        for (int i = 0; i < blue.length; i++) {
+            card[index++] = blue[i];
+        }
+        for (int i = 0; i < green.length; i++) {
+            card[index++] = green[i];
+        }
+        for (int i = 0; i < yellow.length; i++) {
+            card[index++] = yellow[i];
+        }
+        for (int i = 0; i < special.length; i++) {
+            card[index++] = special[i];
         }
     }
 }
